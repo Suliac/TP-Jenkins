005ca2370d6f99402117b558e99ee1d4aaf042e5
Suliac Blineau <suliac.blineau@orange.fr>
2017-04-18 09:42:33 +0200

    refactor the project

---
+++ UnitTests/CalculUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using TP_Jenkins;
+
+namespace UnitTest
+{
+    public class UnitTest
+    {
+        [Test]
+        public void TestAdd()
+        {
+            Assert.AreEqual(TP_Jenkins.Program.add(2, 3), 5);
+        }
+
+        [Test]
+        public void TestDiv()
+        {
+            Assert.AreEqual(TP_Jenkins.Program.div(6, 2), 3);
+        }
+
+        [Test]
+        public void TestDivByZero()
+        {
+            Assert.Throws<DivideByZeroException>(() => TP_Jenkins.Program.div(6, 0));
+        }
+    }
+}
